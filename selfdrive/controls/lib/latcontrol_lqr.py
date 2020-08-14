c84bbfd2203da1b757aa609d555b3cf3bcc496f6
neokii <neokii@gmail.com>
2020-08-14 11:32:19 +0900

    Changed the default lateral values

--- selfdrive/controls/lib/latcontrol_lqr.py
+++ selfdrive/controls/lib/latcontrol_lqr.py
@@ -1,6 +1,6 @@
 import numpy as np
 from selfdrive.controls.lib.drive_helpers import get_steer_max
-from common.numpy_fast import clip
+from common.numpy_fast import clip, interp
 from common.realtime import DT_CTRL
 from cereal import log
 
@@ -55,6 +55,8 @@ class LatControlLQR():
     # Subtract offset. Zero angle should correspond to zero torque
     self.angle_steers_des = path_plan.angleSteers - path_plan.angleOffset
     steering_angle -= path_plan.angleOffset
+
+    torque_scale = min(torque_scale, interp(abs(self.angle_steers_des), [5., 45.], [0.6, 1.2]))
 
     # Update Kalman filter
     angle_steers_k = float(self.C.dot(self.x_hat))
